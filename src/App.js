c6f1faabfd666d867d876cc809887522cf61c9d6
pftom <huan1043269994@gmail.com>
2018-08-29 10:06:21 +0800

    step01

--- src/App.js
+++ src/App.js
@@ -2,7 +2,37 @@ import React, { Component } from "react";
 
 class App extends Component {
   render() {
-    return <div />;
+    const todos = [
+      {
+        title: "先玩一下 mobx",
+        id: 0
+      },
+      {
+        title: "然后做一个网站",
+        id: 1
+      },
+      {
+        title: "网站大热，获得千万投资",
+        id: 2
+      },
+      {
+        title: "成为高富帅，赢取白富美，走上人生巅峰，✌️",
+        id: 3
+      },
+      {
+        title: "想想只是一场梦 😭",
+        id: 4
+      }
+    ];
+    return (
+      <div>
+        <ul>
+          {todos.map(todo => (
+            <li key={todo.id}>{todo.title}</li>
+          ))}
+        </ul>
+      </div>
+    );
   }
 }
 
