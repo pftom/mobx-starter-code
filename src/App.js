468be9303716d64265e28ef76f6ee36189fbaad3
pftom <huan1043269994@gmail.com>
2018-08-29 10:39:07 +0800

    step03

--- src/App.js
+++ src/App.js
@@ -3,29 +3,7 @@ import React, { Component } from "react";
 class App extends Component {
   render() {
     console.log("store", this.props.store);
-    const todos = [
-      {
-        title: "先玩一下 mobx",
-        id: 0
-      },
-      {
-        title: "然后做一个网站",
-        id: 1
-      },
-      {
-        title: "网站大热，获得千万投资",
-        id: 2
-      },
-      {
-        title: "成为高富帅，赢取白富美，走上人生巅峰，✌️",
-        id: 3
-      },
-      {
-        title: "想想只是一场梦 😭",
-        id: 4
-      }
-    ];
-
+    const { todos } = this.props.store;
     return (
       <div>
         <ul>
--- src/index.js
+++ src/index.js
@@ -6,7 +6,28 @@ import App from "./App";
 
 class TodoList {
   @observable
-  todos = [];
+  todos = [
+    {
+      title: "先玩一下 mobx",
+      id: 0
+    },
+    {
+      title: "然后做一个网站",
+      id: 1
+    },
+    {
+      title: "网站大热，获得千万投资",
+      id: 2
+    },
+    {
+      title: "成为高富帅，赢取白富美，走上人生巅峰，✌️",
+      id: 3
+    },
+    {
+      title: "想想只是一场梦 😭",
+      id: 4
+    }
+  ];
 }
 
 const store = new TodoList();
