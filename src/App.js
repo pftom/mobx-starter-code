a03269334a04a6a3b95567d800a0e9a66e8fb229
pftom <huan1043269994@gmail.com>
2018-08-30 15:44:54 +0800

    Back to initial

--- src/App.js
+++ src/App.js
@@ -2,17 +2,7 @@ import React, { Component } from "react";
 
 class App extends Component {
   render() {
-    console.log("store", this.props.store);
-    const { todos } = this.props.store;
-    return (
-      <div>
-        <ul>
-          {todos.map(todo => (
-            <li key={todo.id}>{todo.title}</li>
-          ))}
-        </ul>
-      </div>
-    );
+    return <div />;
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,35 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { observable } from "mobx";
 
 import App from "./App";
 
-class TodoList {
-  @observable
-  todos = [
-    {
-      title: "先玩一下 mobx",
-      id: 0
-    },
-    {
-      title: "然后做一个网站",
-      id: 1
-    },
-    {
-      title: "网站大热，获得千万投资",
-      id: 2
-    },
-    {
-      title: "成为高富帅，赢取白富美，走上人生巅峰，✌️",
-      id: 3
-    },
-    {
-      title: "想想只是一场梦 😭",
-      id: 4
-    }
-  ];
-}
-
-const store = new TodoList();
-
-ReactDOM.render(<App store={store} />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
